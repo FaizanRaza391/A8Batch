9caf94877f35930afc03ba60a0ae92e1868e97b0
faiza <faiza@Faizan>
2025-03-06 16:40:37 +0530

    Second Commit

---
+++ LearningSelenium/src/actionsClass/DoubleClick.java
@@ -0,0 +1,13 @@
+package actionsClass;
+
+import org.openqa.selenium.By;
+
+public class DoubleClick extends BaseClass {
+	public static void main(String[] args) {
+		
+		setUp("chrome", 20, "https://demoapps.qspiders.com/ui/button/buttonDouble?sublist=2");
+		doubleClick(driver, driver.findElement(By.id("btn20")));
+			
+	}
+
+}
